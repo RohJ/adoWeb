c5b2c8f1221578be524418d3d8d1301adcd735b5
RohJ <rohitsjadhav@gmail.com>
2023-12-15 08:51:49 +0530

    WFC1 and WFC2 pages
    Layout completed

--- src/app/website/ministry/wfc2/wfc2.component.ts
+++ src/app/website/ministry/wfc2/wfc2.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import * as Rellax from 'rellax';
 
 @Component({
   selector: 'app-wfc2',
   templateUrl: './wfc2.component.html',
   styleUrls: ['./wfc2.component.scss']
 })
-export class Wfc2Component {
+export class Wfc2Component implements OnInit, AfterViewInit {
+
+  // styles: any[] = [{ "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#e9e9e9" }, { "lightness": 17 }] }, { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 20 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#ffffff" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#ffffff" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#ffffff" }, { "lightness": 16 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }, { "lightness": 21 }] }, { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#dedede" }, { "lightness": 21 }] }, { "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#ffffff" }, { "lightness": 16 }] }, { "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#333333" }, { "lightness": 40 }] }, { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#f2f2f2" }, { "lightness": 19 }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#fefefe" }, { "lightness": 20 }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#fefefe" }, { "lightness": 17 }, { "weight": 1.2 }] }];
+
+  model = {
+    left: true,
+    middle: false,
+    right: false
+};
+
+  constructor(config: NgbCarouselConfig) {
+    config.showNavigationArrows = false;
+		config.showNavigationIndicators = false;
+  }
+
+  ngOnInit() {
+    var rellaxHeader = new Rellax('.rellax-header');
+
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.add('profile-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.add('navbar-transparent');
+  }
+  ngAfterViewInit(){
+    setTimeout(function(){
+      if (window.innerWidth >= 991) {
+          var rellaxHeader = new Rellax('.rellax-header');
+          var rellaxText = new Rellax('.rellax-text');
+      }
+    },200);
+
+  }
+  ngOnDestroy() {
+    var body = document.getElementsByTagName('body')[0];
+    body.classList.remove('profile-page');
+    var navbar = document.getElementsByTagName('nav')[0];
+    navbar.classList.remove('navbar-transparent');
+  }
 
 }
